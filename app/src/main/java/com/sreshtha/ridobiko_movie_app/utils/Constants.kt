56f656240a4cbbad8240bbd1a96d3f2f791f2545
Sreshtha <sreshtha.mehrotra@gmail.com>
2022-02-21 05:53:23 +0530

    constants.kt:added app constants

--- app/src/main/java/com/sreshtha/ridobiko_movie_app/utils/Constants.kt
+++ app/src/main/java/com/sreshtha/ridobiko_movie_app/utils/Constants.kt
@@ -1,4 +1,11 @@
 package com.sreshtha.ridobiko_movie_app.utils
 
-class Constants {
+import com.sreshtha.ridobiko_movie_app.BuildConfig
+
+
+object Constants {
+
+    const val BASE_URL = "http://www.omdbapi.com"
+    const val API_KEY = BuildConfig.API_KEY
+
 }
