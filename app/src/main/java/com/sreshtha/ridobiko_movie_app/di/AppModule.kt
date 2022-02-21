ef90dd062a9ce3821b046d69cae871869fa83311
Sreshtha <sreshtha.mehrotra@gmail.com>
2022-02-21 08:39:01 +0530

    di:created app module for injecting dependencies

--- app/src/main/java/com/sreshtha/ridobiko_movie_app/di/AppModule.kt
+++ app/src/main/java/com/sreshtha/ridobiko_movie_app/di/AppModule.kt
@@ -1,4 +1,30 @@
 package com.sreshtha.ridobiko_movie_app.di
 
+import com.sreshtha.ridobiko_movie_app.api.OMDBapi
+import com.sreshtha.ridobiko_movie_app.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
 class AppModule {
+
+
+    @Provides
+    @Singleton
+    fun providesOMDBapi():OMDBapi{
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(Constants.BASE_URL)
+            .build()
+            .create(OMDBapi::class.java)
+    }
+
 }
