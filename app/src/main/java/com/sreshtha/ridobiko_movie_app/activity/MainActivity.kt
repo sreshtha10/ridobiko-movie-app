1409202492f10549c5688061340603771eb16cb6
Sreshtha <sreshtha.mehrotra@gmail.com>
2022-02-21 08:40:54 +0530

    MainViewModel: created viewModel for actv., MainActivity: moved, added observers

--- app/src/main/java/com/sreshtha/ridobiko_movie_app/activity/MainActivity.kt
+++ app/src/main/java/com/sreshtha/ridobiko_movie_app/activity/MainActivity.kt
@@ -1,16 +1,72 @@
-package com.sreshtha.ridobiko_movie_app
+package com.sreshtha.ridobiko_movie_app.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import com.sreshtha.ridobiko_movie_app.databinding.ActivityMainBinding
+import com.sreshtha.ridobiko_movie_app.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val _viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setUpObservers()
+        initListeners()
+    }
+
+
+    private fun initListeners(){
+
+    }
+
+
+    private fun setUpObservers(){
+        _viewModel.movieData.observe(this) {
+            when (it) {
+                is Resource.Error -> {
+
+                }
+                is Resource.Success -> {
+
+                }
+
+            }
+        }
+
+        _viewModel.seriesData.observe(this) {
+            when (it) {
+                is Resource.Error -> {
+
+                }
+                is Resource.Success -> {
+
+                }
+
+            }
+        }
+
+        _viewModel.showData.observe(this) {
+            when (it) {
+                is Resource.Error -> {
+
+                }
+                is Resource.Success -> {
+
+                }
+
+            }
+        }
+
+
+
+
+
     }
 }
--- app/src/main/java/com/sreshtha/ridobiko_movie_app/activity/MainViewModel.kt
+++ app/src/main/java/com/sreshtha/ridobiko_movie_app/activity/MainViewModel.kt
@@ -1,4 +1,51 @@
 package com.sreshtha.ridobiko_movie_app.activity
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sreshtha.ridobiko_movie_app.api.OMDBapi
+import com.sreshtha.ridobiko_movie_app.model.Show
+import com.sreshtha.ridobiko_movie_app.utils.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(private  val api:OMDBapi) :ViewModel(){
+
+
+    val movieData:MutableLiveData<Resource<Show>> = MutableLiveData()
+    val seriesData:MutableLiveData<Resource<Show>> = MutableLiveData()
+    val showData:MutableLiveData<Resource<Show>> = MutableLiveData()
+
+
+    fun getMovie(title:String){
+        viewModelScope.launch {
+            val result = api.getMovie(title)
+            if(result.isSuccessful){
+                movieData.value = Resource.Success<Show>(result.body())
+            }
+        }
+    }
+
+    fun getSeries(title:String){
+        viewModelScope.launch {
+            val result = api.getSeries(title)
+            if(result.isSuccessful){
+                seriesData.value = Resource.Success<Show>(result.body())
+            }
+        }
+    }
+
+    fun getShow(title:String){
+        viewModelScope.launch {
+            val result = api.getShow(title)
+            if(result.isSuccessful){
+                showData.value = Resource.Success<Show>(result.body())
+            }
+        }
+    }
+
+
 }
