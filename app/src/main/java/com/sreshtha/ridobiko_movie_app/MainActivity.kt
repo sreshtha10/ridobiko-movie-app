9547837adabaa0c496359195fdf094926e6f7098
Sreshtha <sreshtha.mehrotra@gmail.com>
2022-02-21 05:52:31 +0530

    mainActivity.kt:added view binding

--- app/src/main/java/com/sreshtha/ridobiko_movie_app/MainActivity.kt
+++ app/src/main/java/com/sreshtha/ridobiko_movie_app/MainActivity.kt
@@ -2,10 +2,15 @@ package com.sreshtha.ridobiko_movie_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sreshtha.ridobiko_movie_app.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
